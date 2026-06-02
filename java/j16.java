83b3392d3bfe92a3f00a51dea73f1f6987b1e895
sryureddy <reddysarayu650@gmail.com>
2026-06-02 13:31:43 +0530

    added

---
+++ java/j16.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class j16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String cleaned = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if (cleaned.equals(reversed)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
